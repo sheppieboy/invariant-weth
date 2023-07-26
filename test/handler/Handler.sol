19d521bc06db3232bb768ed97987ad5284e22b8f
Luke Sheppard <sheppiegit@gmail.com>
2023-07-26 18:05:36 -0400

    dealt 10 eth to the contract, Handler

--- test/handler/Handler.sol
+++ test/handler/Handler.sol
@@ -2,12 +2,16 @@
 pragma solidity 0.8.18;
 
 import "../../src/WETH9.sol";
+import {CommonBase} from "forge-std/Base.sol";
+import {StdCheats} from "forge-std/StdCheats.sol";
+import {StdUtils} from "forge-std/StdUtils.sol";
 
-contract Handler {
+contract Handler is CommonBase, StdCheats, StdUtils {
     WETH9 public weth;
 
     constructor(WETH9 _weth) {
         weth = _weth;
+        deal(address(weth), 10 ether);
     }
 
     function deposit(uint256 amount) public {
