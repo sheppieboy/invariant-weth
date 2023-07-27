161e5340b55bda1239d4027a9f1b0f76b7854aa6
Luke Sheppard <sheppiegit@gmail.com>
2023-07-27 13:42:24 -0400

    Added a struct and library to use to help count and keep track of addresses balances.

---
+++ test/helpers/AddressSet.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+struct AddressSet {
+    address[] addrs;
+    mapping(address => bool) saved;
+}
+
+library LibAddressSet {
+    function add(AddressSet storage set, address addr) internal {
+        if (set.saved[addr] == false) {
+            set.addrs.push(addr);
+            set.saved[addr] = true;
+        }
+    }
+
+    function contains(AddressSet storage set, address addr) internal view returns (bool) {
+        return set.saved[addr];
+    }
+
+    function count(AddressSet storage set) internal view returns (uint256) {
+        return set.addrs.length;
+    }
+}
